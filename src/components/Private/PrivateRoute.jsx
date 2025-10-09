6575f44a0c74507dcd6be0a9c69ee26075e2bcbd
Guilherme Guaita <guilherme.guaita@softplan.com.br>
2025-10-09 16:59:16 -0300

    refactor: atualizando a chave do token em PrivateRoute para 'authToken'
    Alterou a chave do localStorage de 'token' para 'authToken' em PrivateRoute para garantir que as verificações de autenticação usem o identificador correto do token.

--- src/components/Private/PrivateRoute.jsx
+++ src/components/Private/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("authToken");
   return token ? children : <Navigate to="/Login" replace />;
 };
 
